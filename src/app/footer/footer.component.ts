1ddbcd6adfb9ac27eb6578c2e64b5ced01c2562c
Mariano Antonio Soto Hernandez <mantiniok0051@gmail.com>
2024-05-15 21:04:19 -0600

    Update footer.component.ts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("KreisIndustries FooterComponent")
+  }
 
   ngOnInit() {
     console.log("KreisIndustries FooterComponent")
