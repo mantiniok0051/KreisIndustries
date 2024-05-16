8c9ddb7a14d3f9f7650b711db512453b654b7402
Mariano Antonio Soto Hernandez <mantiniok0051@gmail.com>
2024-05-15 21:05:31 -0600

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,9 +12,11 @@ export class AboutComponent implements OnInit {
   public quote:string = 'Me enseñaron que quien piensa como yo está loco, fué cuando decidí seguir así... ¡Loco!';
   public bio:string = 'I’m up to something. It’s important to use cocoa butter. It’s the key to more success, why not live smooth? Why live rough? Eliptical talk. It’s important to use cocoa butter. It’s the key to more success, why not live smooth? Why live rough? They don’t want us to eat. You see that bamboo behind me though, you see that bamboo? Ain’t nothin’ like bamboo. Bless up. Hammock talk come soon. In life you have to take the trash out.';
   public philosophy:string = 'Let’s see what Chef Dee got that they don’t want us to eat. Another one. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it. Life is what you make it, so let’s make it. Egg whites, turkey sausage, wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there.';
-  constructor() { }
+  constructor() {
+    console.log("KreisIndustries AboutComponent")
+  }
 
   ngOnInit() {
+    console.log("KreisIndustries AboutComponent")
   }
- console.log("About component initiated")
 }
